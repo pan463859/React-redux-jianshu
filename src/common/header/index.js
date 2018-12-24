955f3251e00109345803e3a1287af6beb8d641b6
Grizz <panan521@qq.com>
2018-12-24 20:19:42 +0800

    添加reduce管理数据，组件变成无状态组件

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,59 +1,60 @@
-import React, { Component } from 'react'
-import { SearchWrapper,HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button } from './style'
-import {CSSTransition} from 'react-transition-group';
-class Header extends Component {
-    constructor(props){
-        super(props)
-        this.state={
-            focused:false
-        }
-        this.handleInputFocus=this.handleInputFocus.bind(this) 
-        this.handleInputBlur=this.handleInputBlur.bind(this) 
-    }
-   
-    render() {
-        return (
-            <HeaderWrapper>
-                <Logo href='/' />
-                <Nav>
-                    <NavItem className='left active'>首页</NavItem>
-                    <NavItem className='left'>下载app</NavItem>
-                    <NavItem className='right'>登录</NavItem>
-                    <NavItem className='right'>
-                        <i className='iconfont'>&#xe636;</i>
-                    </NavItem>
-                    <SearchWrapper>
+import React from 'react'
+import { SearchWrapper, HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button } from './style'
+import { CSSTransition } from 'react-transition-group';
+import { connect } from 'react-redux'
+const Header = (props) => {
+    return (
+        <HeaderWrapper>
+            <Logo href='/' />
+            <Nav>
+                <NavItem className='left active'>首页</NavItem>
+                <NavItem className='left'>下载app</NavItem>
+                <NavItem className='right'>登录</NavItem>
+                <NavItem className='right'>
+                    <i className='iconfont'>&#xe636;</i>
+                </NavItem>
+                <SearchWrapper>
                     <CSSTransition
-                    timeout={200}
-                    in={this.state.focused}
-                    classNames="slide"
+                        timeout={200}
+                        in={this.props.focused}
+                        classNames="slide"
                     >
-                    <NavSearch className={this.state.focused?'focused':''} onFocus={this.handleInputFocus} onBlur={this.handleInputBlur} >
-                    </NavSearch>
+                        <NavSearch className={props.focused ? 'focused' : ''} onFocus={props.handleInputFocus} onBlur={this.props.handleInputBlur} >
+                        </NavSearch>
                     </CSSTransition>
                     <i className='iconfont'>  &#xe800;</i>
-                    </SearchWrapper>
-                    <Addition>
-                        <Button className='writing'>
-                            <i className='iconfont'>&#xe632;</i>
-                            写文章</Button>
-                        <Button className='reg'>注册</Button>
-                    </Addition>
-                </Nav>
-            </HeaderWrapper>
-        )
-    }
-    handleInputFocus(){
-        this.setState({
-            focused:true
-        })
-    }
+                </SearchWrapper>
+                <Addition>
+                    <Button className='writing'>
+                        <i className='iconfont'>&#xe632;</i>
+                        写文章</Button>
+                    <Button className='reg'>注册</Button>
+                </Addition>
+            </Nav>
+        </HeaderWrapper>
+    )
 
-    handleInputBlur(){
-        this.setState({
-            focused:false
-        })
-    }
-    
 }
-export default Header;
+const mapStateToProps = (state) => {
+    return {
+        focused: state.focused
+    }
+}
+const mapDispathToProps = (dispatch) => {
+
+    return {
+        handleInputFocus() {
+            const action = {
+                type: 'search_focus'
+            }
+            dispatch(action)
+        },
+        handleInputBlur() {
+            const action = {
+                type: 'search_blur'
+            }
+            dispatch(action)
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispathToProps)(Header);
