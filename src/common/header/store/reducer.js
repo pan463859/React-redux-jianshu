6439526e605e74d1d439940741fcb25199aaea4d
Grizz <panan521@qq.com>
2018-12-25 22:34:51 +0800

    immutable1.0

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -7,14 +7,11 @@ const defaultState = fromJS({
 })
 export default (state = defaultState, action) => {
     if (action.type === constants.SEARCH_FOCUS) {
-        return {
-            focused: true
-        }
+        //immutable对象的set方法，会结合之前的immutable对象的值和设置的值，返回一个新的对象
+        return state.set('focused', true)
     }
     else if (action.type === constants.SEARCH_BlUR) {
-        return {
-            focused: false
-        }
+        return state.set('focused', false)
     }
     return state
 }
